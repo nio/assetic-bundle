75a968b022574bdd352b1f0333fd54345065e1f2
nio <olaf.niemann@gmail.com>
2021-01-21 17:41:03 +0100

    sf52 fix function declaration

--- Twig/AsseticTokenParser.php
+++ Twig/AsseticTokenParser.php
@@ -11,11 +11,12 @@
 
 namespace Symfony\Bundle\AsseticBundle\Twig;
 
-use Assetic\Asset\AssetInterface;
+use Assetic\Contracts\Asset\AssetInterface;
 use Assetic\Extension\Twig\AsseticTokenParser as BaseAsseticTokenParser;
 use Symfony\Bundle\AsseticBundle\Exception\InvalidBundleException;
 use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
 use Twig\Environment;
+use Twig\Node\Node;
 
 /**
  * Assetic token parser.
@@ -63,12 +64,12 @@ class AsseticTokenParser extends BaseAsseticTokenParser
         return parent::parse($token);
     }
 
-    protected function createBodyNode(AssetInterface $asset, \Twig_Node $body, array $inputs, array $filters, $name, array $attributes = array(), $lineno = 0, $tag = null)
+    protected function createBodyNode(AssetInterface $asset, Node $body, array $inputs, array $filters, $name, array $attributes = array(), $lineno = 0, $tag = null)
     {
         return new AsseticNode($asset, $body, $inputs, $filters, $name, $attributes, $lineno, $tag);
     }
 
-    protected function createNode(AssetInterface $asset, \Twig_NodeInterface $body, array $inputs, array $filters, $name, array $attributes = array(), $lineno = 0, $tag = null)
+    protected function createNode(AssetInterface $asset, Node $body, array $inputs, array $filters, $name, array $attributes = array(), $lineno = 0, $tag = null)
     {
         return new AsseticNode($asset, $body, $inputs, $filters, $name, $attributes, $lineno, $tag);
     }
